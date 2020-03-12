6a1ef62a59dd5df4eca4073057350229d7c94a8c
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-03-12 23:40:37 +0000

    idiot

--- rethink-backup.sh
+++ rethink-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /tmp/rethink && rethinkdb dump --file "Rethinkdb Backup $(date +\%F).tar.gz" && && sudo rclone move /tmp/rethink backups:/Databases/Vega --config /home/chris/.config/rclone/rclone.conf
+cd /tmp/rethink && rethinkdb dump --file "Rethinkdb Backup $(date +\%F).tar.gz" && sudo rclone move /tmp/rethink backups:/Databases/Vega --config /home/chris/.config/rclone/rclone.conf
