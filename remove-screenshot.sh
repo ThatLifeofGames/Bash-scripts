30577b799bdc227c7b2c8373a1e41116ff043b14
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-03-14 11:29:13 +0000

    Update remove-screenshot.sh

--- remove-screenshot.sh
+++ remove-screenshot.sh
@@ -7,7 +7,7 @@ read removeImg
 
 if rm /var/www/chris.land/html/i/$removeImg* ; then
         clear
-        echo "Success, check https://chris.land/i/$removeImg for cloudflare cache persistence and remove from backups if needed"
+        echo "Success, check https://chris.land/i/$removeImg for cloudflare cache persistence and remove at https://dash.cloudflare.com/0dad1f233bc5b34886f2174ee3927246/chris.land/caching. Don't forget to remove from backups if needed"
 else
         echo "Error while deleting"
 fi
