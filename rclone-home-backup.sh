6d55be2b0bd3a7ec72b4c4ce8c0d204c590a3f1c
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-06-10 18:34:36 +0100

    Create rclone-home-backup.sh

---
+++ rclone-home-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Backup script to google drive account
+# The github version is missing some folder names, replaced with ???
+#
+
+echo [SCRIPT LOG] Starting backup script at $(date) >> /root/backup.log
+
+/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 8 --create-empty-src-dirs --retries 50 --size-only --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension >> /root/backup.log
+
+echo [SCRIPT LOG] Finished syncing ??? folder at $(date) >> /root/backup.log
+
+/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 1 --retries 50 --create-empty-src-dirs --size-only --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension >> /root/backup.log
+
+echo [SCRIPT LOG] Finished syncing ??? at $(date). Done, exiting. >> /root/backup.log
