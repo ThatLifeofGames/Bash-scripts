25965bec2e516d8c5df404bad6495dc94d4150ff
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-06-26 00:09:37 +0100

    Vega is no more

--- rethink-backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /tmp/rethink && rethinkdb dump --file "Rethinkdb Backup $(date +\%F).tar.gz" && sudo rclone move /tmp/rethink backups:/Databases/Vega --config /home/chris/.config/rclone/rclone.conf
