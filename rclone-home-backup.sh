94288b6e2e99ae365a0a90a3241fb34e3f9c4be6
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-08-18 11:26:45 +0100

    Metadata has been fixed in remote, check by hash

--- rclone-home-backup.sh
+++ rclone-home-backup.sh
@@ -7,10 +7,10 @@
 
 echo [SCRIPT LOG] Starting backup script at $(date) >> /root/backup.log
 
-/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 8 --create-empty-src-dirs --retries 50 --size-only --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
+/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 8 --create-empty-src-dirs --retries 50 --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
 
 echo [SCRIPT LOG] Finished syncing ??? folder at $(date) >> /root/backup.log
 
-/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 1 --retries 50 --create-empty-src-dirs --size-only --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
+/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 1 --retries 50 --create-empty-src-dirs --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
 
 echo [SCRIPT LOG] Finished syncing ??? at $(date). Done, exiting. >> /root/backup.log
