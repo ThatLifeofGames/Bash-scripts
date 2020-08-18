64e369ddd13a2493ccd4308679ee5a46d1016c3c
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-08-19 00:39:46 +0100

    Clean up script

--- rclone-home-backup.sh
+++ rclone-home-backup.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-
-#
-# Backup script to google drive account
-# The github version is missing some folder names, replaced with ???
-#
+ # SOME FOLDER NAMES ARE REMOVED FOR GITHUB!
+########################################
+# Backup script to google drive account#
+######################################## 
 
 echo [SCRIPT LOG] Starting backup script at $(date) >> /root/backup.log
 
-/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 8 --create-empty-src-dirs --retries 50 --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
+/usr/bin/rclone sync /Main/???/ gbackup:/???/\
+ --fast-list \
+ --drive-stop-on-upload-limit \
+ --transfers 8 \
+ --create-empty-src-dirs \
+ --retries 50 \
+ --size-only \
+ --config /root/.config/rclone/rclone.conf \
+ --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ \
+ --suffix $(date +"__%d_%H:%M:%S") \
+ --suffix-keep-extension \
+ --retries-sleep 30s >> /root/backup.log
 
 echo [SCRIPT LOG] Finished syncing ??? folder at $(date) >> /root/backup.log
 
-/usr/bin/rclone sync /Main/???/ gbackup:/???/ --fast-list --drive-stop-on-upload-limit --transfers 1 --retries 50 --create-empty-src-dirs --config /root/.config/rclone/rclone.conf --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries-sleep 30s >> /root/backup.log
+/usr/bin/rclone sync /Main/???/ gbackup:/???/ \
+ --fast-list \
+ --drive-stop-on-upload-limit \
+ --transfers 1 \
+ --retries 50 \
+ --create-empty-src-dirs \
+ --config /root/.config/rclone/rclone.conf \
+ --backup-dir gbackup:/Old/???/$(date +%G)/$(date +%m)/ \
+ --suffix $(date +"__%d_%H:%M:%S") \
+ --suffix-keep-extension \ 
+ --retries-sleep 30s \
+ --progress >> /root/backup.log 
 
 echo [SCRIPT LOG] Finished syncing ??? at $(date). Done, exiting. >> /root/backup.log
