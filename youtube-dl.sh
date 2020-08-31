dfca8e861e100d02bb2de2c6055b4ed9dd4a8efb
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-08-31 15:29:00 +0100

    Don't exit for deleted videos

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -9,11 +9,12 @@ youtube-dl \
 --download-archive ./downloaded-manual.txt \
 --batch-file ./manual.txt \
 --no-overwrites \
+--ignore-errors \
 --continue \
 --add-metadata \
 --write-description \
 --write-thumbnail \
---geo-bypass \
+--geo-bypass \   
 -o "/home/personal/yt-dl/tmp/Archive/%(title)s-%(upload_date)s" \
 --exec 'rclone move /home/personal/yt-dl/tmp/ yt-dl:/ --progress --delete-empty-src-dirs --config /root/.config/rclone/rclone.conf --progress --transfers 8 --drive-chunk-size 64M && echo'
 
@@ -26,11 +27,12 @@ youtube-dl \
 --download-archive ./downloaded-playlists.txt \
 --batch-file ./playlists.txt \
 --no-overwrites \
+--ignore-errors \
 --continue \
 --add-metadata \
 --write-description \
 --write-thumbnail \
---geo-bypass \
+--geo-bypass \   
 -o "/home/personal/yt-dl/tmp/Playlists/%(playlist_title)s/%(title)s-%(uploader)s-%(upload_date)s.%(ext)s" \
 --exec 'rclone move /home/personal/yt-dl/tmp/ yt-dl:/ --progress --delete-empty-src-dirs --config /root/.config/rclone/rclone.conf --progress --transfers 8 --drive-chunk-size 64M && echo'
 
@@ -43,6 +45,7 @@ youtube-dl \
 --download-archive ./downloaded-channels.txt \
 --batch-file ./channels.txt \
 --no-overwrites \
+--ignore-errors \
 --continue \
 --add-metadata \
 --write-description \
