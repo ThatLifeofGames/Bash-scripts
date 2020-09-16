863ca7ae694813f629435786b63854432a77672c
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-09-16 19:07:04 +0100

    Hetzner

--- backup-funkwhale.sh
+++ backup-funkwhale.sh
@@ -3,7 +3,7 @@
 ##############################################################################################
 # Funkwhale database, music (sync) and configuration file backup                             #
 # Crontab entry:                                                                             #
-# 0 2 * * * flock -n /var/lock/funkwhaleBackup.lock -c 'sh /home/ubuntu/backup-funkwhale.sh' #
+# 0 3 * * * flock -n /var/lock/funkwhaleBackup.lock -c 'sh /root/backup-funkwhale.sh'        #
 ##############################################################################################
 
 # Create directory and log to /dev/null in case it complains it exists
@@ -13,17 +13,13 @@ mkdir /tmp/funkwhale-backup > /dev/null
 # Dump postgres datbase data for funkwhale and copy database backup to vps backups remote
 
 sudo -u postgres -H pg_dump funkwhale > /tmp/funkwhale-backup/funkwhale_db_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-rclone move /tmp/funkwhale-backup/ vps-backups:/Funkwhale/Database --retries 50 --config /root/.config/rclone/rclone.conf
+rclone move /tmp/funkwhale-backup/ vps-backups:/Hetzner/Funkwhale/Database --retries 50 --config /root/.config/rclone/rclone.conf
 
 # Sync config file and move old versions to /Old folder
 
-rclone sync /srv/funkwhale/config/.env vps-backups:/Funkwhale/Config --backup-dir vps-backups:/Funkwhale/Old/Configs/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries 50 --config /root/.config/rclone/rclone.conf
-
-# Sync nginx configs
-
-rclone sync /etc/nginx/sites-enabled/ vps-backups:/Funkwhale/Nginx --backup-dir vps-backups:/Funkwhale/Old/Nginx/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --retries 50 --config /root/.config/rclone/rclone.conf --copy-links
+rclone sync /srv/funkwhale/config/.env vps-backups:/Hetzner/Funkwhale/Config --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries 50 --config /root/.config/rclone/rclone.conf
 
 # Sync music/music artwork to vps backups remote keeping old versions in /Old folder
 
-rclone sync /srv/funkwhale/data/media vps-backups:/Funkwhale/media --backup-dir vps-backups:/Funkwhale/Old/Media/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8 
-rclone sync /srv/funkwhale/data/music vps-backups:/Funkwhale/music --backup-dir vps-backups:/Funkwhale/Old/Music/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+rclone sync /srv/funkwhale/data/media vps-backups:/Hetzner/Funkwhale/media --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Media/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+rclone sync /srv/funkwhale/data/music vps-backups:/Hetzner/Funkwhale/music --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Music/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
