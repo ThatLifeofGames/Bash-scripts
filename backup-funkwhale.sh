37764043a990d19ec1a59593ee093d615906401a
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-11-20 12:28:26 +0000

    Remote rename

--- backup-funkwhale.sh
+++ backup-funkwhale.sh
@@ -10,18 +10,18 @@
 
 mkdir /tmp/funkwhale-backup > /dev/null
 
-# Dump postgres datbase data for funkwhale and copy database backup to vps backups remote
+# Dump postgres datbase data for funkwhale and copy database backup to server backups remote
 
 sudo -u postgres -H pg_dump funkwhale > /tmp/funkwhale-backup/funkwhale_db_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-rclone move /tmp/funkwhale-backup/ vps-backups:/Hetzner/Funkwhale/Database --retries 50 --config /root/.config/rclone/rclone.conf
+rclone move /tmp/funkwhale-backup/ server-backups:/Hetzner/Funkwhale/Database --retries 50 --config /root/.config/rclone/rclone.conf
 
 # Sync config file and move old versions to /Old folder
 
-rclone sync /srv/funkwhale/config/.env vps-backups:/Hetzner/Funkwhale/Config --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries 50 --config /root/.config/rclone/rclone.conf
+rclone sync /srv/funkwhale/config/.env server-backups:/Hetzner/Funkwhale/Config --backup-dir server-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries 50 --config /root/.config/rclone/rclone.conf
 
-# Sync music/music artwork to vps backups remote keeping old versions in /Old folder
+# Sync music/music artwork to server backups remote keeping old versions in /Old folder
 
-rclone sync /srv/funkwhale/data/media vps-backups:/Hetzner/Funkwhale/media --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Media/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
-rclone sync /srv/funkwhale/data/music vps-backups:/Hetzner/Funkwhale/music --backup-dir vps-backups:/Hetzner/Funkwhale/Old/Music/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+rclone sync /srv/funkwhale/data/media server-backups:/Hetzner/Funkwhale/media --backup-dir server-backups:/Hetzner/Funkwhale/Old/Media/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+rclone sync /srv/funkwhale/data/music server-backups:/Hetzner/Funkwhale/music --backup-dir server-backups:/Hetzner/Funkwhale/Old/Music/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
 
 rm -rf /tmp/funkwhale-backup/
