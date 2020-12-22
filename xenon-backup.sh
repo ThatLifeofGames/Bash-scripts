6dc03ff3ec6b24b35fc0a6dd84a3a9207b4a5ab2
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2020-12-22 15:17:44 +0000

    Cleanup modifications for new infastructure

--- xenon-backup.sh
+++ xenon-backup.sh
@@ -7,8 +7,14 @@
 
 # Delete old data
 
-/usr/bin/rclone delete xenon:/Xenon-MongoDB/$(date +%d) --drive-use-trash=false --config /root/.config/rclone/rclone.conf >/dev/null 2>&1
+/usr/bin/rclone delete xenon:/Xenon-MongoDB/$(date +%d) \
+--drive-use-trash=false \
+--config /root/.config/rclone/rclone.conf >/dev/null 2>&1
 
 # Dump and upload data via stdout and gzip + archive while uploading
 
-/usr/bin/mongodump --archive --gzip --host 10.0.0.2 --port 27017 | /usr/bin/rclone rcat xenon:/Xenon-MongoDB/$(date +%d)/Dump.archive --drive-chunk-size 64M --bwlimit 30M --drive-use-trash=false --config /root/.config/rclone/rclone.conf >/dev/null 2>&1
+/usr/bin/mongodump --archive --gzip --host 10.0.0.2 --port 27017 | /usr/bin/rclone rcat xenon:/Xenon-MongoDB/$(date +%d)/Dump.archive \
+--drive-chunk-size 64M \
+--bwlimit 30M \
+--drive-use-trash=false \
+--config /root/.config/rclone/rclone.conf >/dev/null 2>&1
