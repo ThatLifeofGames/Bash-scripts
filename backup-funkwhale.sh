3b13c632eb309e8783e5bc17df1c7f9bf9332c37
Chris <35865509+ThatLifeofGames@users.noreply.github.com>
2021-04-27 18:34:39 +0100

    Only retain 30 days of deletions
    Also not keeping suffix because it makes restorations annoying

--- backup-funkwhale.sh
+++ backup-funkwhale.sh
@@ -12,16 +12,23 @@ mkdir /tmp/funkwhale-backup > /dev/null
 
 # Dump postgres datbase data for funkwhale and copy database backup to server backups remote
 
-sudo -u postgres -H pg_dump funkwhale > /tmp/funkwhale-backup/funkwhale_db_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-rclone move /tmp/funkwhale-backup/ server-backups:/Hetzner/Funkwhale/Database --retries 50 --config /root/.config/rclone/rclone.conf
+sudo -u postgres -H pg_dump funkwhale > /tmp/funkwhale-backup/funkwhale_db_`date +%d-%m-%Y"_"%H_%M_%S`.sql # Dump the database
+rclone delete server-backups:/Hetzner/Funkwhale/Database/$(date +%d) --drive-use-trash=false # Delete the previous 30 day old backup
+rclone move /tmp/funkwhale-backup/ server-backups:/Hetzner/Funkwhale/Database/$(date +%d) --retries 50 --config /root/.config/rclone/rclone.conf # Move the new backup
 
 # Sync config file and move old versions to /Old folder
 
-rclone sync /srv/funkwhale/config/.env server-backups:/Hetzner/Funkwhale/Config --backup-dir server-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --retries 50 --config /root/.config/rclone/rclone.conf
+rclone delete server-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%d)/ --drive-use-trash=false # Delete anything removed over a month ago
+rclone sync /srv/funkwhale/config/.env server-backups:/Hetzner/Funkwhale/Config --backup-dir server-backups:/Hetzner/Funkwhale/Old/Configs/$(date +%d)/ --retries 50 --config /root/.config/rclone/rclone.conf
 
 # Sync music/music artwork to server backups remote keeping old versions in /Old folder
 
-rclone sync /srv/funkwhale/data/media server-backups:/Hetzner/Funkwhale/media --backup-dir server-backups:/Hetzner/Funkwhale/Old/Media/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
-rclone sync /srv/funkwhale/data/music server-backups:/Hetzner/Funkwhale/music --backup-dir server-backups:/Hetzner/Funkwhale/Old/Music/$(date +%G)/$(date +%m)/ --suffix $(date +"__%d_%H:%M:%S") --suffix-keep-extension --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+# Delete anything removed over a month ago
+rclone delete server-backups:/Hetzner/Funkwhale/Old/Media/$(date +%d)/ --drive-use-trash=false
+rclone delete server-backups:/Hetzner/Funkwhale/Old/Music/$(date +%d)/ --drive-use-trash=false
+
+# Sync changes, move removed files 
+rclone sync /srv/funkwhale/data/media server-backups:/Hetzner/Funkwhale/media --backup-dir server-backups:/Hetzner/Funkwhale/Old/Media/$(date +%d)/ --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
+rclone sync /srv/funkwhale/data/music server-backups:/Hetzner/Funkwhale/music --backup-dir server-backups:/Hetzner/Funkwhale/Old/Music/$(date +%d)/ --create-empty-src-dirs --drive-stop-on-upload-limit --config /root/.config/rclone/rclone.conf --transfers 8
 
 rm -rf /tmp/funkwhale-backup/
